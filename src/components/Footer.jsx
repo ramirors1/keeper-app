fbc1b50b2a837a87b8d21090b92658f641e448dc
ramirors1 <ramirezr@protonmail.com>
2022-05-30 07:58:41 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    const date = new Date().getFullYear();
+    return (
+        <footer>
+        <p>copyright {date}</p>
+        </footer>
+    );    
+}
+export default Footer;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div className="note">
+            <h1>Note Title</h1>
+            <p>Content of note</p>
+        </div>
+    );    
+}
+export default Note;
